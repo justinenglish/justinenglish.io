4ec3163e990a1db3d59552bf409128522dadabad
Justin English <justnenglish@gmail.com>
2020-01-01 21:07:57 -0500

    Add projects section

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("a").on("click", function(e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      let hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        600,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
