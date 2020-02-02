c4f0ac54278c7140a044c09ca6825d73367fee3b
Justin English <justnenglish@gmail.com>
2020-02-02 17:48:13 -0500

    Initial nav build

---
+++ cheers-to-beers/src/components/navigation/nav.component.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { MainNav, MainNavListItem } from "./nav.styles";
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isTop: true
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 130;
+
+      if (isTop !== this.state.isTop) {
+        this.setState({ isTop });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <MainNav className={this.state.isTop ? "" : "main-nav--fixed"}>
+        <div className="container">
+          <div className="row justify-content-between">
+            <div className="main-nav__logo">
+              <img
+                src={process.env.PUBLIC_URL + "images/beer-logo_v2.png"}
+                alt="Cheers to Beers logo"
+              />
+            </div>
+            <MainNavListItem>
+              <ul>
+                <li>Favorites</li>
+              </ul>
+            </MainNavListItem>
+          </div>
+        </div>
+      </MainNav>
+    );
+  }
+}
+
+export default Nav;
---
+++ cheers-to-beers/src/components/navigation/nav.styles.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const MainNav = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100px;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0);
+  transition: all 0.2s;
+
+  img {
+    width: 115px;
+    height: auto;
+  }
+
+  &.main-nav--fixed {
+    height: 75px;
+    position: fixed;
+    box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.1);
+    background-color: rgba(255, 255, 255, 0.9);
+    backdrop-filter: saturate(180%) blur(20px);
+
+    ul li {
+      font-size: 0.9rem;
+    }
+
+    img {
+      width: 75px;
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    padding: 0 15px;
+  }
+`;
+
+export const MainNavListItem = styled.div`
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+
+  ul {
+    display: flex;
+    list-style: none;
+    align-items: center;
+    padding: 0;
+    margin: 0;
+
+    li:hover {
+      color: orange;
+      cursor: pointer;
+    }
+  }
+`;
