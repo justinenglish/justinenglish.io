7fbac8ca7c377169e7f1d287738fe0f19de5024c
Justin English <justnenglish@gmail.com>
2020-02-08 19:39:37 -0500

    Fetch beer api

--- cheers-to-beers/src/components/cards/cards.component.jsx
+++ cheers-to-beers/src/components/cards/cards.component.jsx
@@ -1,20 +1,43 @@
 import React, { Component } from "react";
-import { CardContainer } from "./cards.styles";
+import {
+  CardContainer,
+  CardSectionTitle,
+  CardTitle,
+  CardIntro,
+  CardTagline
+} from "./cards.styles";
 
 class Cards extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { beers: [] };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    fetch("https://api.punkapi.com/v2/beers")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          beers: data
+        });
+      })
+      .catch(console.log);
+  }
 
   render() {
     return (
-      <CardContainer className="text-center">
-        <h3 style={{ fontSize: "3rem" }}>Cards Header</h3>
-      </CardContainer>
+      <div className="container">
+        <div className="row">
+          {this.state.beers.map(beer => console.log(beer))}
+          <CardContainer>
+            <CardSectionTitle>Card Section Title</CardSectionTitle>
+            <CardTitle></CardTitle>
+            <CardIntro></CardIntro>
+            <CardTagline></CardTagline>
+          </CardContainer>
+        </div>
+      </div>
     );
   }
 }
--- cheers-to-beers/src/components/cards/cards.styles.js
+++ cheers-to-beers/src/components/cards/cards.styles.js
@@ -3,3 +3,19 @@ import styled from "styled-components";
 export const CardContainer = styled.div`
   todo: card styling;
 `;
+
+export const CardSectionTitle = styled.div`
+  todo: card styling;
+`;
+
+export const CardTitle = styled.div`
+  todo: card styling;
+`;
+
+export const CardIntro = styled.div`
+  todo: card styling;
+`;
+
+export const CardTagline = styled.div`
+  todo: card styling;
+`;
